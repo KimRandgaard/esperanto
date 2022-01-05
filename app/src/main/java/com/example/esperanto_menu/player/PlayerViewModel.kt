e5e0965f7d716675302a6597be47e52b39e19915
aqibsajjad <aqib-sajjad@hotmail.com>
2022-01-05 15:14:19 +0100

    afspiler UI

--- app/src/main/java/com/example/esperanto_menu/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/esperanto_menu/player/PlayerViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.esperanto_menu.player
 
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
 import androidx.lifecycle.ViewModel
 
 class PlayerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val binder by lazy {MusicBinder()}
+
+    //override fun onBind(intent: Intent?): IBinder= binder
+
+    inner class MusicBinder : Binder(){
+        fun getService(): PlayerViewModel= this@PlayerViewModel
+    }
+
+
 }
