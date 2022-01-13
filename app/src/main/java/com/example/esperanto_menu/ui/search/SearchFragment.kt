f186c50c500dd7e54d4979c135a26ce8dbc51bc6
Reza <s176481@student.dtu.dk>
2022-01-13 12:08:42 +0100

    Initial search function implemented. Can now search for predefined list of objects. Next step is to bind channels/episodes as searchable items.
    Reza, Andreas Borg, Kim, Jens

--- app/src/main/java/com/example/esperanto_menu/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/esperanto_menu/ui/search/SearchFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.SearchView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -27,15 +29,51 @@ class SearchFragment : Fragment() {
     ): View? {
 
 
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        _binding = FragmentSearchBinding.inflate(inflater,container,false)
+
+        val testARrray = arrayOf("a","b","c","d")
+        val searchAdapter : ArrayAdapter<String> = ArrayAdapter(
+            requireActivity(),android.R.layout.simple_list_item_1,
+            testARrray
+        )
+
+
+        binding.searchList.adapter = searchAdapter
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchView.clearFocus()
+                if (testARrray.contains(query)) {
+
+                    searchAdapter.filter.filter(query)
+                }
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchAdapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+
+
+
         val root: View = binding.root
 
+/*
         val searchView: SearchView = binding.searchView
         viewmodel.text.observe(viewLifecycleOwner, Observer {
             // searchView.text = it
         })
+*/
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
