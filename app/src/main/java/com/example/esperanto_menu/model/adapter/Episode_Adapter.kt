da6f74bbad8cd025b93be0adebfb94b6ebfe87b3
Kim Randgaard <s205341@student.dtu.dk>
2022-01-17 11:59:25 +0100

    specefik episode connection on creation. Kim, Borg, jens, reza

--- app/src/main/java/com/example/esperanto_menu/model/adapter/Episode_Adapter.kt
+++ app/src/main/java/com/example/esperanto_menu/model/adapter/Episode_Adapter.kt
@@ -1,24 +1,25 @@
 package com.example.esperanto_menu.model.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.esperanto_menu.R
 import com.example.esperanto_menu.model.adapter.Episode_Adapter.ViewHolder
 import com.example.esperanto_menu.model.data.Channel
+import com.example.esperanto_menu.ui.episodes.EpisodesFragmentsDirections
 import com.example.esperanto_menu.util.getDuration
 
 class Episode_Adapter(
-    private val context: Context,
-    private val values1: List<Channel>
-    )
-    : RecyclerView.Adapter<ViewHolder>() {
-
+private val context: Context,
+private val values: List<Channel>
+) : RecyclerView.Adapter<Episode_Adapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val episodeInflater = LayoutInflater.from(context)
@@ -28,28 +29,33 @@ class Episode_Adapter(
 
     }
 
-    override fun onBindViewHolder(newholder: ViewHolder, newposition: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.bind(item)
 
-        val newItem = values1[newposition]
-        newholder.Bind(newItem)
+        holder.itemView.setOnClickListener {
+            val action = EpisodesFragmentsDirections.actionNavigationEpisodesToNavigationSpeceficEpisode(episodeName = item.plennomo)
 
+            holder.view.findNavController().navigate(action)
+        }
     }
 
-    override fun getItemCount(): Int = values1.size
+    override fun getItemCount(): Int = values.size
 
-    class ViewHolder(private val itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
-        private val teksto = itemView.findViewById<TextView>(R.id.episodeDescription)
-        private val episodeName = itemView.findViewById<TextView>(R.id.episodeName)
-        private val episodeLength = itemView.findViewById<TextView>(R.id.episodeLenght)
 
-        fun Bind(item: Channel){
+        private val teksto = view.findViewById<TextView>(R.id.episodeDescription)
+        private val episodeName = view.findViewById<TextView>(R.id.episodeName)
+        private val episodeLength = view.findViewById<TextView>(R.id.episodeLenght)
+
+        fun bind(item: Channel){
             teksto.text = item.teksto
             episodeName.text = item.plennomo +" "+ item.dato
-           // episodeLength.text = getDuration(item.mp3fajlo)
+            // episodeLength.text = getDuration(item.mp3fajlo)
 
         }
-
     }
+
 }
+
